85a66300f8689fb0d2b762943e0955808bbf1fb9
david <david.gross@thp.uni-koeln.de>
2025-11-22 00:31:42 +0100

    cleanup

--- Mathlib/LinearAlgebra/PiTensorProductOfSet.lean
+++ Mathlib/LinearAlgebra/PiTensorProductOfSet.lean
@@ -337,34 +337,10 @@ variable {κ : Type*} {Sf : κ → Set ι} [hd : ∀ i, ∀ x, Decidable (x ∈ 
 variable (H : Pairwise fun k l => Disjoint (Sf k) (Sf l))
 variable {M : κ → Type*} [∀ k, AddCommMonoid (M k)] [∀ k, Module R (M k)]
 
-/-- Given a family `(k : κ) → Sf` of disjoint sets and a family of linear maps
+/- Given a family `(k : κ) → Sf` of disjoint sets and a family of linear maps
 where `L k` is defined on tensors indexed by `Sf k`, construct a linear map
 defined on tensors indexed by the union of `Sf`. -/
 
-open Classical in -- for decidable instances, remove later
-def unifyMaps (L : (k : κ) → ((⨂[R] i : Sf k, s i) →ₗ[R] (M k))) :
-  (⨂[R] i : iUnion Sf, s i) →ₗ[R] (⨂[R] k, M k) := lift {
-    toFun x := ⨂ₜ[R] k, (L k) (⨂ₜ[R] i : Sf k, x ⟨i, by aesop⟩)
-    map_update_add' := by
-      intro _ f i a b
-
-      -- `Warning: hi assumption is actually false, because the sets are disjoint`
-      -- `but for demonstrating that even if the inner tensor product is broken down`
-      -- `the goal remains unprovable, it is assumed`
-      have hi : ∀ k, i.val ∈ Sf k := sorry
-      have h : ∀ k : κ, ∀ x, (fun j : (Sf k) => Function.update f i x ⟨j, by aesop⟩) =
-          Function.update (fun j : Sf k => f ⟨↑j, by aesop⟩) ⟨i, hi k⟩ x := by
-        aesop (add safe unfold Function.update)
-
-      conv => lhs; arg 2; intro k; simp [h k]
-      conv => rhs; arg 1; arg 2; intro k; simp [h k]
-      conv => rhs; arg 2; arg 2; intro k; simp [h k]
-
-
-
-
-    map_update_smul' := sorry
-  }
 -- TBD: prove & merge with above
 /-- Given a family `(k : κ) → Sf` of disjoint sets, there is a multilinear map
 from maps on tensors indexed by `Sf k` to tensors indexed by the union of `Sf`. -/
