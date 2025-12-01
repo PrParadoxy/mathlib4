3230c71de3e4fe90c9e1baf6553bdb14f83c41aa
DHTT <prparadoxes@gmail.com>
2025-12-01 22:09:22 +0100

    .

--- Mathlib/LinearAlgebra/PiTensorProduct/Phelps/Dual2.lean
+++ Mathlib/LinearAlgebra/PiTensorProduct/Phelps/Dual2.lean
@@ -3,10 +3,9 @@ import Mathlib.LinearAlgebra.Dual.Lemmas
 import Mathlib.Analysis.Normed.Field.Lemmas
 
 
-open Module Topology
+open Module Topology WeakBilin
 
-variable (R : Type*) [CommSemiring R] [TopologicalSpace R]
-  [T2Space R] [ContinuousConstSMul R R] [ContinuousAdd R]
+variable (R : Type*) [CommSemiring R]
 variable (V : Type*) [AddCommGroup V] [Module R V]
 
 /-- A type synonym for `Dual ℝ W`, equipping it with weak topology. -/
@@ -40,5 +39,33 @@ theorem isCompact_image_dualembed {s : Set (AlgWeakDual R V)} {c : Set (V → R)
   dualembed_isclosed_embedding.isCompact_iff.mpr
     (IsCompact.of_isClosed_subset hc
       (dualembed_isclosed_embedding.isClosed_iff_image_isClosed.mp hs) hsc)
+
+
+
+
+namespace AlgWeakDual
+
+variable {R : Type*} [Field R]
+variable {V : Type*} [AddCommGroup V] [Module R V]
+
+theorem exists_dual_vec_ne_zero :
+    ∀ v: V, v ≠ 0 → ∃ dv: AlgWeakDual R V, dv v ≠ 0 := fun v hv => by
+  obtain ⟨g, hg⟩ := LinearMap.exists_extend
+    (LinearPMap.mkSpanSingleton (K := R) v (1: R) (hv)).toFun
+  use g
+  intro hc
+  have hp := LinearMap.congr_fun hg ⟨v, Submodule.mem_span_singleton_self v⟩
+  erw [LinearPMap.toFun_eq_coe] at hp
+  simp [hc] at hp
+
+variable [TopologicalSpace R] [ContinuousAdd R] [ContinuousConstSMul R R] [IsTopologicalAddGroup R]
+
+theorem WeakBilin.eval_dualpairing_injective :
+    Function.Injective ((eval (dualPairing R V))) := by
+  apply LinearMap.ker_eq_bot.mp (LinearMap.ker_eq_bot'.mpr ?_)
+  intro v hv
+  by_contra! hc
+  obtain ⟨dv, hdv⟩ := exists_dual_vec_ne_zero (R := R) v hc
+  exact hdv (congrArg (fun f => f dv) hv)
 
 
