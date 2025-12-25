b413b0cd2afd85a2932eefb4cd8cbccfc8b07e60
David Gross <david.gross@thp.uni-koeln.de>
2025-12-25 23:04:04 +0100

    golfed projectiveSeminorm_tprod

--- Mathlib/LinearAlgebra/PiTensorProduct/projectiveSeminorm_tprod.lean
+++ Mathlib/LinearAlgebra/PiTensorProduct/projectiveSeminorm_tprod.lean
@@ -1,6 +1,6 @@
+import Mathlib.LinearAlgebra.PiTensorProduct.Dual
 import Mathlib.Analysis.Normed.Module.PiTensorProduct.ProjectiveSeminorm
 import Mathlib.Analysis.Normed.Module.HahnBanach
-
 
 
 
@@ -13,6 +13,30 @@ variable {ι : Type uι} [Fintype ι]
 variable {𝕜 : Type u𝕜} [NontriviallyNormedField 𝕜]
 variable {E : ι → Type uE} [∀ i, NormedAddCommGroup (E i)] [∀ i, NormedSpace 𝕜 (E i)]
 
+section golfed
+
+variable {𝕜 : Type u𝕜} [RCLike 𝕜]
+variable {E : ι → Type uE} [∀ i, NormedAddCommGroup (E i)] [∀ i, NormedSpace 𝕜 (E i)]
+
+theorem projectiveSeminorm_tprod_golfed (m : Π i, E i) :
+    projectiveSeminorm (⨂ₜ[𝕜] i, m i) = ∏ i, ‖m i‖ := by
+  apply eq_of_le_of_ge (projectiveSeminorm_tprod_le m)
+  haveI := nonempty_subtype.mpr (nonempty_lifts (⨂ₜ[𝕜] i, m i))
+  apply le_ciInf (fun x ↦ ?_)
+  choose g hg₁ hg₂ using fun i ↦ exists_dual_vector'' 𝕜 (m i)
+  have hx := congr_arg (norm ∘ dualDistrib (tprod 𝕜 (g ·))) ((mem_lifts_iff _ _).mp x.prop)
+  simp only [Function.comp_apply, dualDistrib_apply, ContinuousLinearMap.coe_coe, hg₂, norm_prod,
+    norm_algebraMap', norm_norm, map_list_sum, List.map_map] at hx
+  grw [←hx, List.le_sum_of_subadditive norm norm_zero.le norm_add_le] -- define `norm_list_sum_le`?
+  simp only [List.map_map, projectiveSeminormAux]
+  apply List.sum_le_sum (fun p hp ↦ ?_)
+  simp only [Function.comp_apply, map_smul, dualDistrib_apply, ContinuousLinearMap.coe_coe,
+    smul_eq_mul, norm_mul, norm_prod]
+  gcongr
+  grw [ContinuousLinearMap.le_opNorm, hg₁]
+  simp
+
+end golfed
 
 noncomputable def liftedLinearfamily (g : (i : ι) → StrongDual 𝕜 (E i))
     : (⨂[𝕜] i, E i) →ₗ[𝕜] 𝕜 := lift {
