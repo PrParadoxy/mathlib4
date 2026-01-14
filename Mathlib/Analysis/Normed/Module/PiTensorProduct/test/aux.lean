297fbad33dec87cd0297a3ea1e6c02727bd12383
DHTT <prparadoxes@gmail.com>
2026-01-15 00:49:54 +0100

    .

---
+++ Mathlib/Analysis/Normed/Module/PiTensorProduct/test/aux.lean
@@ -0,0 +1,12 @@
+import Mathlib.LinearAlgebra.TensorProduct.Basis
+import Mathlib.LinearAlgebra.Basis.VectorSpace
+
+open scoped TensorProduct
+open Module
+
+variable (R : Type*) (M : Type*) (N : Type*)
+variable [CommSemiring R]
+variable [AddCommMonoid M] [Module R M]
+variable [AddCommMonoid N] [Module R N]
+
+
