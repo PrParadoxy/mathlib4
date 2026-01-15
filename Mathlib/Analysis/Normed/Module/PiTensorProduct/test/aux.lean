91b4ec2ff07d292f534507cd900d8de9b03c4ddb
DHTT <prparadoxes@gmail.com>
2026-01-15 02:22:23 +0100

    .

--- Mathlib/Analysis/Normed/Module/PiTensorProduct/test/aux.lean
+++ Mathlib/Analysis/Normed/Module/PiTensorProduct/test/aux.lean
@@ -3,14 +3,11 @@ import Mathlib.LinearAlgebra.Basis.VectorSpace
 import Mathlib.LinearAlgebra.Dual.Lemmas
 
 open scoped TensorProduct
-open Module Submodule
+open Module Submodule Free
 
 
-variable (R : Type*) [DivisionRing R]
-variable {M : Type*} [AddCommGroup M] [Module R M]
-variable {N : Type*} [AddCommGroup N] [Module R N]
-
-theorem exists_dual_vec_ne_zero :
+theorem exists_dual_vec_ne_zero (R : Type*) {M : Type*}
+    [DivisionRing R] [AddCommGroup M] [Module R M] :
     ∀ v : M, v ≠ 0 → ∃ dv : Dual R M, dv v ≠ 0 := fun v hv => by
   obtain ⟨g, hg⟩ := LinearMap.exists_extend
     (LinearPMap.mkSpanSingleton (K := R) v (1: R) (hv)).toFun
@@ -20,3 +17,15 @@ theorem exists_dual_vec_ne_zero :
   simp [hc] at hp
 
 
+variable {R : Type*} {S : Type*} {M : Type*} {N : Type*}
+  [CommSemiring R] [Semiring S] [Algebra R S] [AddCommMonoid M] [Module R M]
+  [Module S M] [IsScalarTower R S M] [Module.Free S M]
+  [AddCommMonoid N] [Module R N] [Module.Free R N]
+
+lemma TensorProduct.as_sum_on_basis (x : M ⊗[R] N) :
+    let b := Module.Free.chooseBasis S (M ⊗[R] N)
+    x = ∑ i ∈ (b.repr x).support, b.repr x i • b i := by
+  intro b
+  nth_rw 1 [← b.linearCombination_repr x, Finsupp.linearCombination_apply S (b.repr x)]
+  exact Finsupp.sum_of_support_subset (b.repr x) (fun _ a ↦ a) _ (by simp)
+  
