4d22d3ca7f0f231954800341ad30b579ec05bbbc
DHTT <prparadoxes@gmail.com>
2026-01-16 01:16:46 +0100

    .

--- Mathlib/Analysis/Normed/Module/PiTensorProduct/test/aux.lean
+++ Mathlib/Analysis/Normed/Module/PiTensorProduct/test/aux.lean
@@ -10,7 +10,7 @@ theorem exists_dual_vec_ne_zero (R : Type*) {M : Type*}
     [DivisionRing R] [AddCommGroup M] [Module R M] :
     ∀ v : M, v ≠ 0 → ∃ dv : Dual R M, dv v ≠ 0 := fun v hv => by
   obtain ⟨g, hg⟩ := LinearMap.exists_extend
-    (LinearPMap.mkSpanSingleton (K := R) v (1: R) (hv)).toFun
+    (LinearPMap.mkSpanSingleton (K := R) v (1 : R) (hv)).toFun
   use g, fun hc => ?_
   have hp := LinearMap.congr_fun hg ⟨v, mem_span_singleton_self v⟩
   rw [LinearPMap.toFun_eq_coe] at hp
@@ -19,13 +19,37 @@ theorem exists_dual_vec_ne_zero (R : Type*) {M : Type*}
 
 variable {R : Type*} {S : Type*} {M : Type*} {N : Type*}
   [CommSemiring R] [Semiring S] [Algebra R S] [AddCommMonoid M] [Module R M]
-  [Module S M] [IsScalarTower R S M] [Module.Free S M]
-  [AddCommMonoid N] [Module R N] [Module.Free R N]
+  [Module S M] [IsScalarTower R S M]
+  [AddCommMonoid N] [Module R N]
 
-lemma TensorProduct.as_sum_on_basis (x : M ⊗[R] N) :
-    let b := Module.Free.chooseBasis S (M ⊗[R] N)
-    x = ∑ i ∈ (b.repr x).support, b.repr x i • b i := by
-  intro b
-  nth_rw 1 [← b.linearCombination_repr x, Finsupp.linearCombination_apply S (b.repr x)]
-  exact Finsupp.sum_of_support_subset (b.repr x) (fun _ a ↦ a) _ (by simp)
-  
+lemma as_sum_on_basis [Module.Free R N] [Module.Free S M] (x : M ⊗[R] N) :
+    let bm := chooseBasis S M
+    let bn := chooseBasis R N
+    let b := Basis.tensorProduct bm bn
+    x = ∑ i ∈ (b.repr x).support, (b.repr x) i • (bm i.1 ⊗ₜ[R] bn i.2) := by
+  intro bn bm b
+  nth_rw 1 [← b.linearCombination_repr x, Finsupp.linearCombination_apply S (b.repr x),
+    Finsupp.sum_of_support_subset (b.repr x) (fun _ a ↦ a) _ (by simp)]
+  congr with _
+  simp [b, Module.Basis.tensorProduct_apply']
+
+
+
+
+variable {R : Type*} {S : Type*} {M : Type*} {N : Type*}
+  [CommSemiring R] [Semiring S] [Algebra R S] [AddCommMonoid M] [Module R M]
+  [Module S M] [IsScalarTower R S M]
+  [AddCommMonoid N] [Module R N]
+
+
+lemma TensorProduct.eq_zero_of_dual_apply_sum_eq_zero
+    [Module.Free R N] [Module.Free S M] (u : M ⊗[R] N) :
+    let bm := chooseBasis S M
+    let bn := chooseBasis R N
+    let b := Basis.tensorProduct bm bn
+    (∀ ψ : Dual R N, ∑ i ∈ (b.repr u).support, ψ (bn i.2) • bm i.1 = 0) → u = 0 := by
+  intro bm bn b
+  contrapose!
+  intro hu
+  by_cases hi : ∃ i : ChooseBasisIndex S M × ChooseBasisIndex R N, bm i.1 ≠ 0
+  .
