9844cc3b886c10b5ee981cb5ecd8d442b7938601
DHTT <prparadoxes@gmail.com>
2026-01-17 19:42:08 +0100

    .

--- Mathlib/Analysis/Normed/Module/PiTensorProduct/test/aux.lean
+++ Mathlib/Analysis/Normed/Module/PiTensorProduct/test/aux.lean
@@ -123,10 +123,9 @@ open Set ContinuousLinearMap
 
 
 theorem opNorm_IsLUB (f : E →SL[σ₁₂] F) : IsLUB (Set.range (fun x : E ↦ ‖f x‖ / ‖x‖)) ‖f‖ := by
-  constructor
-  · intro _ ; aesop (add safe forward ratio_le_opNorm)
-  · intro _ hb
-    simp only [mem_upperBounds, mem_range, forall_exists_index, forall_apply_eq_imp_iff] at hb
+  refine ⟨fun _ ↦ ?_, fun _ hb ↦ ?_⟩
+  · aesop (add safe forward ratio_le_opNorm)
+  · simp only [mem_upperBounds, mem_range, forall_exists_index, forall_apply_eq_imp_iff] at hb
     refine opNorm_le_bound' f (by simpa [mem_upperBounds] using hb 0) (fun e _ => ?_)
     grw [←div_le_iff₀ (by positivity)]
     exact hb e
