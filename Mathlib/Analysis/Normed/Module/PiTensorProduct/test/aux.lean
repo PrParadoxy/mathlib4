100dd72e6cfaf6e69d956765fe44dc3b0d23d9df
DHTT <prparadoxes@gmail.com>
2026-01-20 20:44:31 +0100

    .

--- Mathlib/Analysis/Normed/Module/PiTensorProduct/test/aux.lean
+++ Mathlib/Analysis/Normed/Module/PiTensorProduct/test/aux.lean
@@ -10,7 +10,7 @@ open Module Submodule Free
 
 theorem exists_dual_vec_ne_zero (R : Type*) {M : Type*}
     [DivisionRing R] [AddCommGroup M] [Module R M] :
-    ∀ v : M, v ≠ 0 → ∃ dv : Dual R M, dv v ≠ 0 := fun v hv => by
+    ∀ v, v ≠ 0 → ∃ dv : Dual R M, dv v ≠ 0 := fun v hv => by
   obtain ⟨g, hg⟩ := LinearMap.exists_extend
     (LinearPMap.mkSpanSingleton (K := R) v (1 : R) (hv)).toFun
   use g, fun hc => ?_
@@ -24,6 +24,8 @@ variable {R : Type*} {S : Type*} {M : Type*} {N : Type*} {ι : Type*} {κ : Type
   [Module S M] [IsScalarTower R S M]
   [AddCommMonoid N] [Module R N]
 
+
+
 lemma as_sum_on_basis (bm : Basis ι S M) (bn : Basis κ R N) (x : M ⊗[R] N) :
     x = ∑ i ∈ ((bm.tensorProduct bn).repr x).support, ((bm.tensorProduct bn).repr x)
       i • (bm i.1 ⊗ₜ[R] bn i.2) := by
@@ -34,26 +36,32 @@ lemma as_sum_on_basis (bm : Basis ι S M) (bn : Basis κ R N) (x : M ⊗[R] N) :
   simp [b, Module.Basis.tensorProduct_apply']
 
 
-
+variable {R : Type*} {S : Type*} {M : Type*} {N : Type*} {ι : Type*} {κ : Type*}
+  [Field R] [Semiring S] [Algebra R S] [AddCommMonoid M] [Module R M]
+  [Module S M] [IsScalarTower R S M]
+  [AddCommGroup N] [Module R N]
+set_option maxHeartbeats 0
 lemma eq_zero_of_dual_apply_sum_eq_zero (bm : Basis ι S M) (bn : Basis κ R N) (x : M ⊗[R] N) :
     (∀ ψ : Dual R N, ∑ i ∈ ((bm.tensorProduct bn).repr x).support, ψ (bn i.2) • bm i.1 = 0)
     → x = 0 := by
-  by_cases hi : ∃ i : ι × κ, bm i.1 ≠ 0
+  by_cases hi : ∃ i : ι × κ, bn i.2 ≠ 0
   · contrapose!
     intro hx
     obtain ⟨i, hi⟩ := hi
-    
-
+    obtain ⟨ψ, hψ⟩ := exists_dual_vec_ne_zero R (bn i.2) hi
+    use ψ
+    apply?
 
   · intros
     rw [as_sum_on_basis bm bn x]
     apply Finset.sum_eq_zero (fun x hx => ?_)
     simp only [not_exists, not_not] at hi
-    rw [hi x, TensorProduct.zero_tmul, smul_zero]
+    rw [hi x, TensorProduct.tmul_zero, smul_zero]
 
 
 
-
+variable (bn : Basis κ R N) [DecidableEq κ] [Finite κ]
+#check bn.dualBasis
 
 
 
