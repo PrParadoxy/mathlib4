a1ff14977171a60e9d3fd65127fac7cb8a7c188a
DHTT <prparadoxes@gmail.com>
2026-01-21 00:11:21 +0100

    .

--- Mathlib/Analysis/Normed/Module/PiTensorProduct/test/aux.lean
+++ Mathlib/Analysis/Normed/Module/PiTensorProduct/test/aux.lean
@@ -15,12 +15,12 @@ lemma support_basis_nonempty {R M ι : Type*} [Semiring R] [AddCommMonoid M] [Mo
 
 
 variable {R : Type*} {S : Type*} {M : Type*} {N : Type*} {ι : Type*} {κ : Type*}
-  [CommSemiring R] [Semiring S] [Algebra R S] [AddCommMonoid M] [Module R M]
+  [CommSemiring R] [Semiring S] [Nontrivial S] [Algebra R S] [AddCommMonoid M] [Module R M]
   [Module S M] [IsScalarTower R S M] [AddCommMonoid N] [Module R N]
 
 open Basis
 
-lemma eq_zero_of_dual_apply_sum_eq_zero (bm : Basis ι S M) (bn : Basis κ R N) (x : M ⊗[R] N) :
+lemma eq_zero_of_forall_dual_eq_zero (bm : Basis ι S M) (bn : Basis κ R N) (x : M ⊗[R] N) :
     (∀ ψ : Dual R N, ∑ i ∈ ((bm.tensorProduct bn).repr x).support, ψ (bn i.2) • bm i.1 = 0)
     → x = 0 := by
   contrapose!
@@ -35,89 +35,14 @@ lemma eq_zero_of_dual_apply_sum_eq_zero (bm : Basis ι S M) (bn : Basis κ R N) 
     aesop (add safe forward Finsupp.single_apply)
   · simp_all
 
+theorem eq_zero_of_forall_dual_eq_zero_free [Module.Free R N] [Module.Free S M] (x : M ⊗[R] N) :
+    let bm := chooseBasis S M
+    let bn := chooseBasis R N
+    let b := bm.tensorProduct bn
+    (∀ ψ : Dual R N, ∑ i ∈ (b.repr x).support, ψ (bn i.2) • bm i.1 = 0) → x = 0 :=
+  eq_zero_of_forall_dual_eq_zero _ _ _
 
 
 
 
 
-
-
-  -- contrapose!
-  -- intro hx
-  -- obtain ⟨i, hi⟩ := basis_support_nonempty x (bm.tensorProduct bn) hx
-  -- use bn.coord i.2
-  -- classical
-  -- simp only [Basis.coord_apply, Basis.repr_self]
-  -- conv_lhs => arg 2; ext x; rw [single_eq_indicator, indicator_apply]
-  -- simp only [mem_singleton, dite_eq_ite, ite_smul, one_smul, zero_smul, ne_eq]
-  -- erw [Finset.sum_dite]
-  -- simp only [Finset.sum_const_zero, add_zero]
-
-
-  -- by_cases hi : ∃ i : ι × κ, bn i.2 ≠ 0
-  -- · contrapose!
-  --   intro hx
-  --   obtain ⟨i, hi⟩ := hi
-
-
-  -- · intros
-  --   rw [as_sum_on_basis bm bn x]
-  --   apply Finset.sum_eq_zero (fun x hx => ?_)
-  --   simp only [not_exists, not_not] at hi
-  --   rw [hi x, TensorProduct.tmul_zero, smul_zero]
-
-
-
-
-
--- lemma as_sum_on_basis_free [Module.Free R N] [Module.Free S M] (x : M ⊗[R] N) :
---     let bm := chooseBasis S M
---     let bn := chooseBasis R N
---     let b := Basis.tensorProduct bm bn
---     x = ∑ i ∈ (b.repr x).support, (b.repr x) i • (bm i.1 ⊗ₜ[R] bn i.2) := by
---   intro bn bm b
---   nth_rw 1 [← b.linearCombination_repr x, Finsupp.linearCombination_apply S (b.repr x),
---     Finsupp.sum_of_support_subset (b.repr x) (fun _ a ↦ a) _ (by simp)]
---   congr with _
---   simp [b, Module.Basis.tensorProduct_apply']
-
-
-
-
--- variable {R : Type*} {S : Type*} {M : Type*} {N : Type*}
---   [CommSemiring R] [Semiring S] [Algebra R S] [AddCommMonoid M] [Module R M]
---   [Module S M] [IsScalarTower R S M]
---   [AddCommMonoid N] [Module R N]
-
--- lemma eq_zero_of_dual_apply_sum_eq_zero
---     [Module.Free R N] [Module.Free S M] (u : M ⊗[R] N) :
---     let bm := chooseBasis S M
---     let bn := chooseBasis R N
---     let b := Basis.tensorProduct bm bn
---     (∀ ψ : Dual R N, ∑ i ∈ (b.repr u).support, ψ (bn i.2) • bm i.1 = 0) → u = 0 := by
---   intro bm bn b
---   by_cases hi : ∃ i : ChooseBasisIndex S M × ChooseBasisIndex R N, bm i.1 ≠ 0
---   . sorry
---   · intro h
---     rw [as_sum_on_basis (R := R) (S := S) u]
---     simp only [ne_eq, Prod.exists, exists_const_iff, exists_and_left, not_and, not_exists, not_not,
---       Nonempty.forall] at hi
---     apply Finset.sum_eq_zero
---     intro x hx
---     rw [hi x.2 x.1]
---     simp
-
-
--- variable {R : Type*} {S : Type*} {M : Type*} {N : Type*} {ι : Type*} {κ : Type*}
---   [CommSemiring R] [Semiring S] [Algebra R S] [AddCommMonoid M] [Module R M]
---   [Module S M] [IsScalarTower R S M]
---   [AddCommMonoid N] [Module R N]
-
--- lemma as_sum_on_basis (bm : Basis ι S M) (bn : Basis κ R N) (x : M ⊗[R] N) :
---     x = ∑ i ∈ ((bm.tensorProduct bn).repr x).support, ((bm.tensorProduct bn).repr x)
---       i • (bm i.1 ⊗ₜ[R] bn i.2) := by
---   let b := bm.tensorProduct bn
---   nth_rw 1 [← b.linearCombination_repr x, Finsupp.linearCombination_apply S (b.repr x),
---     Finsupp.sum_of_support_subset (b.repr x) (fun _ a ↦ a) _ (by simp)]
---   congr with _
---   simp [b, Module.Basis.tensorProduct_apply']
